1c662b5d4216f728f6bf0f5a7c01a67756a55652
FrankBjelland <fogvshop@gmail.com>
2026-06-13 14:13:39 +0200

    Add screen structure for App.js split

---
+++ mobile/screens/InterviewScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function InterviewScreen() {
+  return (
+    <View>
+      <Text>InterviewScreen placeholder</Text>
+    </View>
+  );
+}
